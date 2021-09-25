7c647a3c7c2ba6ec8a0be1988ce1af6323fac703
qwertyuio123456 <91388863+qwertyuio123456@users.noreply.github.com>
2021-09-26 01:35:49 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,19 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    titleWeb: "Chào con mèo",
+    introTitle: 'Hnt.tien',
+    introDesc: `Tình yêu của tui như 
+    phú ông viết di chúc
+    Không muốn ai nhận ngoài cậu cả`,
+    btnIntro: '^^Hihi^^',
+    title: 'Gặp được em là điều hạnh phúc nhất cuộc đời anh 🥰',
+    desc: 'Nụ cười của em như đường kính vì nó xuyên tâm ',
+    btnYes: 'Thích lắm <3',
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    reply: 'Yêu thì yêu omà không yêu thì yêu <3',
+    mess: 'Anh biết mà. Yêu em nhiều nhiều, Muahhhhhh',
+    messDesc: 'Nào hết dịch anh qua đón đi chơi nha.',
+    btnAccept: 'Okeee luonnn <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
